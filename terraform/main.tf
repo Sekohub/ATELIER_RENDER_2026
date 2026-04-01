c131e114ba2e6bdc3f5080767dab879c50f82647
SerkanHub <ozerserkan05@gmail.com>
2026-04-01 14:39:30 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,12 +17,6 @@ variable "github_actor" {
   type        = string
 }
 
-env_vars = {
-  ENV = {
-    value = "production"
-  }
-}
-
 resource "render_web_service" "flask_app" {
   name   = "flask-render-iac-${var.github_actor}"
   plan   = "free"
