c5890e0667ae3cda04b65b990d9cb7283b3f9c69
SerkanHub <ozerserkan05@gmail.com>
2026-04-01 17:51:20 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://atelier-render-2026-3zq1.onrender.com/")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
   return (
     <div>
       <h1>React + Flask + PostgreSQL 🚀</h1>
+      <p>{message}</p>
     </div>
   );
 }
